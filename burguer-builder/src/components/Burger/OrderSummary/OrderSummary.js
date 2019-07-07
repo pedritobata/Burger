54e4d75cedeffe1d39904eb8061a0d0308cbcc0f
unknown <pedromartinez_goyo@hotmail.com>
2019-07-06 20:14:51 -0500

    Using componentWillUpdate to improve performance of Modal/OrderSummary

--- burguer-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burguer-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../../hoc/Auxiliar';
 import Button from '../../UI/Button/Button';
 
-const OderSummary = (props) => {
+//Este componente lo hemos vuelto del tipo class solo para usar los metodos del lifecycle
+//En realidad debería ser un componente funcional!!!
+class OderSummary extends Component {
 
-     const ingredientSummary = Object.keys(props.ingredients)
-     .map(igkey => {
-        return (<li key={igkey}>
-            <span style={{textTransform: 'capitalize'}}>{igkey}</span>: {props.ingredients[igkey]}
-            </li>)
-     }) ;
+    //Implementamos este metodo solo para debuggear
+    componentWillUpdate(){
+        console.log('[OrderSummary] willUpdate');
+    }
 
-     return (
-        <Aux>
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
+        .map(igkey => {
+           return (<li key={igkey}>
+               <span style={{textTransform: 'capitalize'}}>{igkey}</span>: {this.props.ingredients[igkey]}
+               </li>)
+        }) ;
+
+        return (
+            <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+            <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Aux>
-     );
-};
+        );
+    }
+}
 
 
 export default OderSummary;
--- burguer-builder/src/components/UI/Modal/Modal.js
+++ burguer-builder/src/components/UI/Modal/Modal.js
@@ -1,22 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Auxiliar';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-  <Aux>
-   <Backdrop show={props.show} clicked={props.modalClosed}/>
+//Este componente lo hemos vuelto del tipo class solo para usar los metodos del lifecycle
+class Modal extends Component{
 
- <div className={classes.Modal}
-       style={{
-         transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-         opacity : props.show? '1' : '0'
-       }}>
-      {props.children}
-  </div>
-  </Aux>
- 
-);
+  shouldComponentUpdate(nextProps, nextState){
+     //trataremos de hacer que el modal no se actualice cuando no se esté visualizando
+     //ya que no tiene sentido
+    return nextProps.show !== this.props.show;
+  }
 
-export default modal;
+  componentWillUpdate(){
+    console.log('[Modal] willUpdate');
+  }
+
+  render(){
+
+    return(
+      <Aux>
+      <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+   
+       <div className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity : this.props.show? '1' : '0'
+          }}>
+         {this.props.children}
+       </div>
+     </Aux>
+    );
+  }
+} 
+
+export default Modal;
