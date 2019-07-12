d603909c63c8b64e32054ada27381a48c0d988a6
unknown <pedromartinez_goyo@hotmail.com>
2019-07-12 15:36:48 -0500

    Removing interceptors when not used by hoc withErrorHandler

--- burguer-builder/src/hoc/withErrorHandler/withErrorHandler.js
+++ burguer-builder/src/hoc/withErrorHandler/withErrorHandler.js
@@ -7,7 +7,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
 
         state = {
-            error : null
+            error : null,
         }
 
         //Usamos componentWillMount en vez de componentDidMount porque
@@ -16,14 +16,23 @@ const withErrorHandler = (WrappedComponent, axios) => {
         //para que los interceptors funcionen tambien para ellos!!!
         //Tambien se podría usar el constructor para esto
         componentWillMount(){
-            axios.interceptors.request.use(req => {
+            //creamos los interceptors y los guardamos como propiedades del state del componente
+            this.requestInterceptor = axios.interceptors.request.use(req => {
                 this.setState({error : null});
                 return req;
             })
 
-            axios.interceptors.response.use(res => res, error => {
+            this.responseInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({error : error});
             });
+        }
+
+        //usamos esta funcion del lifecycle para quitar los interceptors que se hayan creado al usar
+        //este componente una vez que el componente ya no se use, ya que si re utilizamos este componente en varias ocasiones para envolver otros
+        //componentes, entonces los interceptores se seguirían creando y acumulando en vano!!
+        componentWillUnmount(){
+            axios.interceptors.request.eject(this.requestInterceptor);
+            axios.interceptors.response.eject(this.responseInterceptor);
         }
 
         errorConfirmedHandler = () => {
